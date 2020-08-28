887f58e26263fa00a7c1ba4aace9d65672086594
e <602476523@qq.com>
2020-08-28 17:34:05 +0800

    crj20200828

---
+++ web/src/components/app/App.vue
@@ -0,0 +1,145 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-08-11 16:27:05
+ * @LastEditTime: 2020-08-20 14:36:32
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \常州平台\web\src\components\home\AppCenter.vue
+-->
+<!--  -->
+<template>
+  <div class="bg-white">
+    <div class="w  py-5 ">
+      <div class="d-flex jc-left ai-end">
+        <h2 class="mr-3 fs-xxl-2">研发设计</h2>
+        <p class="mr-5 text-grey">划线价格为产品历时销售价而非原价</p>
+        <div class="fs-md btn">
+          <!-- <span
+            :class="{active: isActive == index}"
+            v-for="(item, index) in list"
+            :key="index"
+            @click="isActive = index"
+          >{{item}}</span> -->
+        </div>
+      </div>
+      <div class="more d-flex mt-5">
+        <div class="left p-3 text-white">
+          <div>
+            <h3 class="fs-xxl border-bottom py-2 mb-2">欧盟RoHS测试</h3>
+            <p>
+              RoHS一站式解决方案
+              <br />增强产品绿色竞争力
+            </p>
+            <span class="hot">hot</span>
+          </div>
+          <div class="show text-center w-100">立即查看</div>
+        </div>
+        <div class="right">
+          <div class="d-flex flex-wrap goods-list">
+            <div class="p-4" v-for="(item, index) in 6" :key="index">
+              <h3 class="fs-xxl">服装鞋包热门测试套餐</h3>
+              <p class="my-2 text-grey">电商平台入驻,太保天猫京东认可</p>
+              <div class="w-100 text-center">
+                <img src="@/assets/img/ad1.jpg"  alt />
+              </div>
+              <span class="hot">hot</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      list: ["研发设计1", "研发设计2", "研发设计3", "研发设计4"],
+      isActive: 0,
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  methods: {},
+};
+</script>
+<style scoped lang="scss">
+.btn {
+  span {
+    // display: inline-block;
+    margin-right: 15px;
+    padding: 10px 25px;
+    color: #999;
+    border: 1px solid #999;
+    cursor: pointer;
+    &.active {
+      color: #1875f0;
+      border-color: #1875f0;
+    }
+  }
+}
+.left {
+  width: 25%;
+  height: 415px;
+  background-color: red;
+  background: url("../../assets/img/ad.jpg") no-repeat;
+  background-size: cover;
+
+  > div {
+    position: relative;
+    width: 130px;
+    // background-color: #fff;
+  }
+  p {
+    line-height: 18px;
+  }
+  .show {
+    margin-top: 260px;
+    padding: 8px 0;
+    border: 1px solid #fff;
+  }
+}
+.right {
+  flex: 1;
+  //   background-color: green;
+  .goods-list {
+    height: 100%;
+    > div {
+      // flex-shrink: 1;
+      position: relative;
+      width: 33.3%;
+      height: 50%;
+      line-height: 30px;
+      border: 1px solid #eee;
+      background-color: #fff;
+      transition: 0.5s all;
+
+      &:hover {
+        // transform: scale(1.1);
+        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.3);
+        z-index: 1000;
+      }
+      .hot {
+        top: 15px;
+        right: 25px;
+        line-height: 1.5;
+      }
+    }
+  }
+}
+.hot {
+  position: absolute;
+  top: 0;
+  right: -30px;
+  padding: 2px 6px;
+  border-radius: 5px;
+  color: #fff;
+  background-color: orange;
+}
+
+</style>
--- web/src/views/app/appindex.vue
+++ web/src/views/app/appindex.vue
@@ -1,26 +1,239 @@
 <template>
-    <div class="appindex">
-        <section>
-  <div class="bannnerInfo">
-    <div class="banner-wrap">
-      <div class="bannnerLeft">
-        <p class="bannnerTitle">应用商城</p>
-        <p class="bannnerDetail">为企业提供工业互联网一体化数字管理能力</p>
+  <div class="appindex">
+    <section>
+      <div class="bannnerInfo">
+        <Banner :imgList="imgList" class="bannner" />
+        <div class="category">
+          <ul>
+            <li id="caregroytitle">应用分类</li>
+            <li v-for="item in categorylist" class="caregroylist">
+              <div class="aside"></div>
+              <!-- <div class="recommend">
+                <ul>
+                  <li v-for="item in apprecommend">
+                    <div class="appitem">
+                      <div class="img">
+                        <img :src="item.url" alt width="100px" height="70px" />
+                      </div>
+                      <p>{{item.name}}</p>
+                    </div>
+                  </li>
+                </ul>
+              </div> -->
+              <el-row :gutter="20">
+                <el-col :span="6">
+                  <div class="icon">
+                    <i :class="item.icon"></i>
+                  </div>
+                </el-col>
+                <el-col :span="18">
+                  <div class="categoryname">{{item.name}}</div>
+                  <div class="categorylabel">{{item.label}}</div>
+                  <!-- <p>{{item.label}}</p> -->
+                </el-col>
+              </el-row>
+            </li>
+          </ul>
+        </div>
+
+        <div class="recommend">
+          <ul>
+            <li v-for="item in apprecommend">
+              <div class="appitem">
+                <div class="img">
+                  <img :src="item.url" alt width="100px" height="70px" />
+                </div>
+                <p>{{item.name}}</p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="bannerRight"><img src=""></div>
-    </div>
+    </section>
+    <section>
+      <App />
+      <div class="line w"></div>
+    </section>
+    <section>
+      <App />
+      <div class="line"></div>
+    </section>
+    <section>
+      <App />
+      <div class="line"></div>
+    </section>
+    <section>
+      <App />
+      <div class="line"></div>
+    </section>
+    <section>
+      <App />
+    </section>
   </div>
-</section>
-    </div>
 </template>
 
 <script>
+import Banner from "../../components/Banner";
+import img1 from "../../assets/img/app/banner1.jpg";
+import img2 from "../../assets/img/app/banner2.png";
+import img from "../../assets/img/app/app1.jpg";
+import App from "../../components/app/App";
+
 export default {
-    name:"AppIndex"
-    
-}
+  name: "AppIndex",
+  components: {
+    Banner,
+    App,
+  },
+  data() {
+    return {
+      imgList: [img1, img2],
+      categorylist: [
+        {
+          name: "研发设计",
+          label: "提升效率，降低成本",
+          icon: "el-icon-s-tools",
+        },
+        {
+          name: "研发设计",
+          label: "提升效率，降低成本",
+          icon: "el-icon-s-tools",
+        },
+        {
+          name: "研发设计",
+          label: "提升效率，降低成本",
+          icon: "el-icon-s-tools",
+        },
+        {
+          name: "研发设计",
+          label: "提升效率，降低成本",
+          icon: "el-icon-s-tools",
+        },
+        {
+          name: "研发设计",
+          label: "提升效率，降低成本",
+          icon: "el-icon-s-tools",
+        },
+        {
+          name: "研发设计",
+          label: "提升效率，降低成本",
+          icon: "el-icon-s-tools",
+        },
+      ],
+      apprecommend: [
+        { name: "产线仿真建模", url: img },
+        { name: "产线仿真建模", url: img },
+        { name: "产线仿真建模", url: img },
+        { name: "产线仿真建模", url: img },
+      ],
+    };
+  },
+  methods: {},
+};
 </script>
 
 <style lang="scss" scoped>
+.appindex {
+  background: white;
+}
+.bannnerInfo {
+  height: 550px;
+  position: relative;
+}
+.category {
+  position: absolute;
+  top: 0;
+  left: 100px;
+  // background: cadetblue;
+  z-index: 2;
 
+  font-size: 16px;
+  color: white;
+  width: 300px;
+  background: #374766;
+}
+
+.category li {
+  height: 80px;
+  line-height: 80px;
+  background: #374766;
+  padding: 0 20px;
+}
+
+.caregroylist:hover .aside {
+  transform: scale(1);
+  opacity: 0.4;
+}
+
+.caregroylist {
+  position: relative;
+  border-bottom: 1px solid rgba($color: #eeeeee, $alpha: 0.5);
+}
+.caregroylist:nth-last-child(1) {
+  border-bottom: none;
+}
+#caregroytitle {
+  background: #0077ff;
+  height: 70px;
+  line-height: 70px;
+}
+.categoryname {
+  height: 40px;
+  line-height: 40px;
+}
+.categorylabel {
+  height: 40px;
+  line-height: 40px;
+  font-size: 14px;
+  color: #dddddd;
+}
+.icon {
+  font-size: 26px;
+}
+
+.aside {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  transition: all 0.4s;
+  width: 100%;
+  height: 100%;
+  background: #3480dd;
+  transform: scale(0);
+}
+
+.recommend {
+  width: 200px;
+  z-index: 2;
+  position: absolute;
+  bottom: 0;
+  left: 400px;
+  background: white;
+  display: none;
+}
+
+.caregroylist:hover .recommend {
+  display: block;
+}
+
+.appitem {
+  padding: 10px;
+  border-bottom: 1px solid rgba($color: #dddddd, $alpha: 0.5);
+}
+.img {
+  width: 100px;
+  height: 70px;
+  margin: auto;
+}
+.appitem p {
+  line-height: 30px;
+  height: 30px;
+  text-align: center;
+}
+.line {
+  background: #eeeeee;
+  height: 3px;
+  width: 1150px;
+  margin: auto;
+}
 </style>
